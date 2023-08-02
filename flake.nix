dbf0b65edc36fb0720a83f9d2509afe50ee63846
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-08-02 11:03:40 +0200

    Instantiate `python-project-template`

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "kontrol - Foundry integration for KEVM";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    let
+      allOverlays = [
+        poetry2nix.overlay
+        (final: prev: {
+          kontrol = prev.poetry2nix.mkPoetryApplication {
+            python = prev.python310;
+            projectDir = ./.;
+            groups = [];
+            # We remove `dev` from `checkGroups`, so that poetry2nix does not try to resolve dev dependencies.
+            checkGroups = [];
+           };
+        })
+      ];
+    in flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = allOverlays;
+        };
+      in {
+        packages = rec {
+          inherit (pkgs) kontrol;
+          default = kontrol;
+        };
+      }) // {
+        overlay = nixpkgs.lib.composeManyExtensions allOverlays;
+      };
+}
