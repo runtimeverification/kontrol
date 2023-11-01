1cb6b93b2f90d18c53aa00b9b31ecdb9eac5820d
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-11-01 08:15:05 +0100

    Rename `foundryx` in comment

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
                 cmake
                 # This is somewhat hacky but it's only a build time dependency.
                 # We basically override kevm-pyk to add kontrol as a runtime dependency
-                # so that kevm-dist finds the foundryx target.
+                # so that kevm-dist finds the foundry target.
                 (prev.kevm-pyk.overridePythonAttrs (old: {
                   propagatedBuildInputs = (old.propagatedBuildInputs or [ ])
                     ++ [
