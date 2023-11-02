157622ae4fd825471c394b4adabdaf201255de5b
Lisandra Silva <lisandra@iMac.local>
2023-11-02 10:28:37 +0000

    format

--- src/kontrol/__main__.py
+++ src/kontrol/__main__.py
@@ -79,7 +79,7 @@ def _check_k_version() -> None:
     expected_k_version = KVersion.parse(f'v{pyk.K_VERSION}')
     actual_k_version = k_version()
 
-    if not _compare_versions(expected_k_version, actual_k_version) :
+    if not _compare_versions(expected_k_version, actual_k_version):
         _LOGGER.warning(f'K version {expected_k_version} was expected but K version {actual_k_version} is being used.')
 
 
