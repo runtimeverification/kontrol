7ac73215ceb5963a09acb376c235cdb91b0b8630
Juan C <juanconrod@protonmail.com>
2023-11-14 18:02:39 +0100

    Fix quotation

--- src/kontrol/solc_to_k.py
+++ src/kontrol/solc_to_k.py
@@ -410,11 +410,11 @@ class Contract:
 
     @staticmethod
     def contract_to_module_name(c: str) -> str:
-        return Contract.escaped(c, "S2K") + '-CONTRACT'
+        return Contract.escaped(c, 'S2K') + '-CONTRACT'
 
     @staticmethod
     def contract_to_verification_module_name(c: str) -> str:
-        return Contract.escaped(c, "S2K") + '-VERIFICATION'
+        return Contract.escaped(c, 'S2K') + '-VERIFICATION'
 
     @staticmethod
     def test_to_claim_name(t: str) -> str:
