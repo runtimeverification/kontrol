ab81edc89aaf728a9045b4233d08a76870424d54
palinatolmach <polina.tolmach@gmail.com>
2023-12-05 22:06:39 +0800

    Code quality fix

--- src/kontrol/solc_to_k.py
+++ src/kontrol/solc_to_k.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import json
 import logging
 import re
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from functools import cached_property
 from subprocess import CalledProcessError
 from typing import TYPE_CHECKING
