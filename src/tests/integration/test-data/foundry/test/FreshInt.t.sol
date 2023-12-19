9b3c505ed925c4b8a3678108ae81903f5914034d
Noah Watson <noah@nwatson.xyz>
2023-12-19 13:54:41 -0600

    Restore other fresh cheatcode tests

--- src/tests/integration/test-data/foundry/test/FreshInt.t.sol
+++ src/tests/integration/test-data/foundry/test/FreshInt.t.sol
@@ -5,9 +5,27 @@ import "forge-std/Test.sol";
 import "../src/KEVMCheats.sol";
 
 contract FreshIntTest is Test, KEVMCheats {
+    int128 constant min = -170141183460469231731687303715884105728;
+    int128 constant max = 170141183460469231731687303715884105727;
 
     bytes1 local_byte;
     bytes local_bytes;
+
+    function test_uint128() public {
+        uint256 fresh_uint128 = uint256(kevm.freshUInt(32));
+        assertGe(fresh_uint128, type(uint128).min);
+        assertLe(fresh_uint128, type(uint128).max);
+    }
+
+    function test_bool() public {
+        uint256 fresh_uint256 = kevm.freshBool();
+        assertGe(fresh_uint256, 0);
+        assertLe(fresh_uint256, 1);
+    }
+
+    function testFail_int128() public {
+        int128 val = int128(uint128(kevm.freshUInt(16)));
+        assertGt(val, max);
 
     function test_bytes() public {
         bytes memory fresh_bytes_1 = kevm.freshBytes(5);
