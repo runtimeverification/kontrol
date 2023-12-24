a4768b635f69ce40e34250c6c31a7793b8f958a1
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-12-24 19:42:01 +0000

    kontrol/prove: make sure to break on foundry calls/returns to not generate those rules for KEVM

--- src/kontrol/prove.py
+++ src/kontrol/prove.py
@@ -207,15 +207,16 @@ def _run_cfg_group(
                 use_gas=prove_options.use_gas,
             )
 
+            cut_point_rules = KEVMSemantics.cut_point_rules(prove_options.break_on_jumpi, prove_options.break_on_calls)
+            cut_point_rules += ['FOUNDRY-CHEAT-CODES.foundry.call', 'FOUNDRY-CHEAT-CODES.foundry.return']
+
             run_prover(
                 foundry.kevm,
                 proof,
                 kcfg_explore,
                 max_depth=prove_options.max_depth,
                 max_iterations=prove_options.max_iterations,
-                cut_point_rules=KEVMSemantics.cut_point_rules(
-                    prove_options.break_on_jumpi, prove_options.break_on_calls
-                ),
+                cut_point_rules=cut_point_rules,
                 terminal_rules=KEVMSemantics.terminal_rules(prove_options.break_every_step),
                 counterexample_info=prove_options.counterexample_info,
             )
