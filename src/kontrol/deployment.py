e79688bd987e27873fadf51e15fbd9f28ba691a9
Lucas MT <lucas.tabajara@runtimeverification.com>
2024-01-18 17:30:37 -0300

    Change visibility of vm in deployment summary to private

--- src/kontrol/deployment.py
+++ src/kontrol/deployment.py
@@ -97,9 +97,9 @@ class DeploymentSummary:
         # Appending the cheatcode address to be able to avoid extending `Test`
         lines.append('\t// Cheat code address, 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D')
         lines.append(
-            '\taddress internal constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));'
+            '\taddress private constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));'
         )
-        lines.append('\tVm internal constant vm = Vm(VM_ADDRESS);\n')
+        lines.append('\tVm private constant vm = Vm(VM_ADDRESS);\n')
 
         for acc_key in list(self.accounts):
             lines.append('\taddress internal constant ' + self.accounts[acc_key] + 'Address = ' + acc_key + ';')
