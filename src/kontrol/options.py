e088b8f652d1dc22e0633372a873d88f98cfb1b8
Noah Watson <noah@nwatson.xyz>
2024-02-14 11:22:05 -0600

    Remove options from KontrolProveOption and inherit from KEVM ProveOptions

--- src/kontrol/options.py
+++ src/kontrol/options.py
@@ -10,54 +10,23 @@ if TYPE_CHECKING:
     from pyk.utils import BugReport
 
     from .deployment import SummaryEntry
+    from kevm_pyk.options import ProveOptions
 
 
 @dataclass(frozen=True)
-class ProveOptions:
-    auto_abstract_gas: bool
-    bug_report: BugReport | None
+class KontrolProveOptions(ProveOptions):
     bmc_depth: int | None
-    max_depth: int
-    break_every_step: bool
-    break_on_jumpi: bool
-    break_on_calls: bool
-    break_on_storage: bool
-    break_on_basic_blocks: bool
     break_on_cheatcodes: bool
-    workers: int
-    counterexample_info: bool
-    max_iterations: int | None
     run_constructor: bool
-    fail_fast: bool
-    reinit: bool
     use_gas: bool
-    failure_info: bool
     summary_entries: Iterable[SummaryEntry] | None
-    fast_check_subsumption: bool
-    always_check_subsumption: bool
-    post_exec_simplify: bool
-    fallback_on: Iterable[str | FallbackReason] | None
-    interim_simplification: int | None
 
     def __init__(
         self,
         *,
-        auto_abstract_gas: bool | None = None,
-        bug_report: BugReport | None = None,
         bmc_depth: int | None = None,
-        max_depth: int | None = None,
-        break_every_step: bool | None = None,
-        break_on_jumpi: bool | None = None,
-        break_on_calls: bool | None = None,
-        break_on_storage: bool | None = None,
-        break_on_basic_blocks: bool | None = None,
         break_on_cheatcodes: bool | None = None,
-        workers: int | None = None,
-        counterexample_info: bool | None = None,
-        max_iterations: int | None = None,
         run_constructor: bool | None = None,
-        fail_fast: bool | None = None,
-        reinit: bool | None = None,
         use_gas: bool | None = None,
         failure_info: bool | None = None,
         summary_entries: list[SummaryEntry] | None = None,
@@ -66,73 +35,11 @@ class ProveOptions:
         post_exec_simplify: bool | None = None,
         fallback_on: Iterable[str | FallbackReason] | None = None,
         interim_simplification: int | None = None,
+        **kwargs,
     ) -> None:
-        object.__setattr__(self, 'auto_abstract_gas', bool(auto_abstract_gas))
-        object.__setattr__(self, 'bug_report', bug_report)
         object.__setattr__(self, 'bmc_depth', bmc_depth)
-        object.__setattr__(self, 'max_depth', 1000 if max_depth is None else max_depth)
-        object.__setattr__(self, 'break_every_step', bool(break_every_step))
-        object.__setattr__(self, 'break_on_jumpi', bool(break_on_jumpi))
-        object.__setattr__(self, 'break_on_calls', bool(break_on_calls))
-        object.__setattr__(self, 'break_on_storage', bool(break_on_storage))
-        object.__setattr__(self, 'break_on_basic_blocks', bool(break_on_basic_blocks))
         object.__setattr__(self, 'break_on_cheatcodes', bool(break_on_cheatcodes))
-        object.__setattr__(self, 'workers', 1 if workers is None else workers)
-        object.__setattr__(self, 'counterexample_info', True if counterexample_info is None else counterexample_info)
-        object.__setattr__(self, 'max_iterations', max_iterations)
         object.__setattr__(self, 'run_constructor', bool(run_constructor))
-        object.__setattr__(self, 'fail_fast', True if fail_fast is None else fail_fast)
-        object.__setattr__(self, 'reinit', bool(reinit))
         object.__setattr__(self, 'use_gas', bool(use_gas))
-        object.__setattr__(self, 'failure_info', True if failure_info is None else failure_info)
         object.__setattr__(self, 'summary_entries', summary_entries)
-        object.__setattr__(
-            self, 'fast_check_subsumption', True if fast_check_subsumption is None else fast_check_subsumption
-        )
-        object.__setattr__(
-            self, 'always_check_subsumption', True if always_check_subsumption is None else always_check_subsumption
-        )
-        object.__setattr__(self, 'post_exec_simplify', True if post_exec_simplify is None else post_exec_simplify)
-        object.__setattr__(self, 'fallback_on', fallback_on)
-        object.__setattr__(self, 'interim_simplification', interim_simplification)
-
-
-@dataclass(frozen=True)
-class RPCOptions:
-    use_booster: bool
-    kore_rpc_command: tuple[str, ...]
-    smt_timeout: int | None
-    smt_retry_limit: int | None
-    smt_tactic: str | None
-    trace_rewrites: bool
-    port: int | None
-    maude_port: int | None
-
-    def __init__(
-        self,
-        *,
-        use_booster: bool | None = None,
-        kore_rpc_command: str | Iterable[str] | None = None,
-        smt_timeout: int | None = None,
-        smt_retry_limit: int | None = None,
-        smt_tactic: str | None = None,
-        trace_rewrites: bool | None = None,
-        port: int | None = None,
-        maude_port: int | None = None,
-    ) -> None:
-        if kore_rpc_command is None:
-            kore_rpc_command = (
-                ('kore-rpc-booster',) if (True if use_booster is None else use_booster) else ('kore-rpc',)
-            )
-        elif isinstance(kore_rpc_command, str):
-            kore_rpc_command = (kore_rpc_command,)
-        else:
-            kore_rpc_command = tuple(kore_rpc_command)
-        object.__setattr__(self, 'use_booster', True if use_booster is None else use_booster)
-        object.__setattr__(self, 'kore_rpc_command', kore_rpc_command)
-        object.__setattr__(self, 'smt_timeout', smt_timeout)
-        object.__setattr__(self, 'smt_retry_limit', smt_retry_limit)
-        object.__setattr__(self, 'smt_tactic', smt_tactic)
-        object.__setattr__(self, 'trace_rewrites', bool(trace_rewrites))
-        object.__setattr__(self, 'port', port)
-        object.__setattr__(self, 'maude_port', maude_port)
+        super().__init__(kwargs)
