6395371c0a1e5030e14eab13fc09f99e793a8ee8
Petar Maksimovic <petar.maksimovic@runtimeverification.com>
2024-02-21 01:43:15 +0000

    more general tests

--- src/tests/integration/test-data/foundry/test/FreshBytes.t.sol
+++ src/tests/integration/test-data/foundry/test/FreshBytes.t.sol
@@ -8,28 +8,35 @@ contract FreshBytesTest is Test, KEVMCheats {
     bytes1 local_byte;
     bytes local_bytes;
 
+    uint256 constant length_limit = 48;
+
     function manip_symbolic_bytes(bytes memory b) public {
         uint middle = b.length / 2;
         b[middle] = hex'aa';
     }
 
     function test_symbolic_bytes_1() public {
-        bytes memory fresh_bytes_1 = kevm.freshBytes(5);
-        local_byte = fresh_bytes_1[0];
-        assertEq(fresh_bytes_1[0], local_byte);
+        uint256 length = uint256(kevm.freshUInt(1));
+        vm.assume (0 < length);
+        vm.assume (length <= length_limit);
+        bytes memory fresh_bytes = kevm.freshBytes(length);
+        uint256 index = uint256(kevm.freshUInt(1));
+        vm.assume(index < length);
 
-        bytes memory fresh_bytes_2 = kevm.freshBytes(67);
-        assertEq(fresh_bytes_2.length, 67);
-        local_bytes = fresh_bytes_2;
-        assertEq(fresh_bytes_2, local_bytes);
+        local_byte = fresh_bytes[index];
+        assertEq(fresh_bytes[index], local_byte);
+
+        local_bytes = fresh_bytes;
+        assertEq(fresh_bytes, local_bytes);
+
+        manip_symbolic_bytes(fresh_bytes);
+        assertEq(hex'aa', fresh_bytes[length / 2]);
     }
 
     function test_symbolic_bytes_length(uint256 l) public {
-        vm.assume(l <= 48);
-        vm.assume(l > 0);
+        vm.assume(0 < l);
+        vm.assume(l <= length_limit);
         bytes memory fresh_bytes = kevm.freshBytes(l);
-        manip_symbolic_bytes(fresh_bytes);
-        assertEq(hex'aa', fresh_bytes[l / 2]);
         assertEq(fresh_bytes.length, l);
     }
 }
