39a8f492970b59e3c656c7798f83c4615ac878e9
devops <devops@runtimeverification.com>
2024-02-28 12:27:40 +0000

    flake.{nix,lock}: update Nix derivations

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kontrol";
 
   inputs = {
-    kevm.url = "github:runtimeverification/evm-semantics/v1.0.464";
+    kevm.url = "github:runtimeverification/evm-semantics/v1.0.465";
     nixpkgs.follows = "kevm/nixpkgs";
     nixpkgs-pyk.follows = "kevm/nixpkgs-pyk";
     k-framework.follows = "kevm/k-framework";
