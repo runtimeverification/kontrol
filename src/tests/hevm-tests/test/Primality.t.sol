1abe78452a2225dc59e04e037c176659db59458d
lisandrasilva <lisandrasilva18@hotmail.com>
2024-02-28 14:39:51 +0000

    Moved hevm tests to integration tests

--- src/tests/hevm-tests/test/Primality.t.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.13;
-
-contract PrimalityCheck{
-    function factor(uint x, uint y) public pure {
-      require(1 < x && x < 973013 && 1 < y && y < 973013);
-      assert(x*y != 973013);
-    }
-}
