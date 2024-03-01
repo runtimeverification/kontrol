a2dbd41f9914bc72e25bbfcfe74de3e71b563421
lisandrasilva <lisandrasilva18@hotmail.com>
2024-03-01 11:57:21 +0000

    Refactored tests

--- src/tests/integration/test-data/foundry/test/HevmTests.t.sol
+++ src/tests/integration/test-data/foundry/test/HevmTests.t.sol
@@ -3,27 +3,27 @@ pragma solidity ^0.8.13;
 
 import {console, Test} from "forge-std/Test.sol";
 
-contract SimpleTest is Test{
+contract HevmTests is Test{
 
   // BYTECODE=$(jq .deployedBytecode.object -r out/Simple.t.sol/SimpleTest.json)
 
-    function prove_assertEq(uint x) public {
+    function prove_assertEq_true(uint x) public {
       // Passes with `hevm symbolic --code $BYTECODE --sig "prove_assertEq(uint)"`
       // Passes with `hevm test` 
       require(x < 1);
       assertEq(x, 0);
     }
 
-    function prove_assertEq_2(uint x) public {
+    function prove_assertEq_false(uint x) public {
       // Passes with `hevm symbolic --code $BYTECODE --sig "prove_assertEq_2(uint)"`
       // Fails with `hevm test` 
       require(x < 1);
       assertEq(x, 1);
     }
 
-    function proveFail_assertEq_2(uint x) public {
+    function proveFail_assertEq(uint x) public {
       // Passes with `hevm symbolic --code $BYTECODE --sig "proveFail_assertEq_2(uint)"`
-      // Passes with `hevm test` 
+      // Passes with `hevm test`
       require(x < 1);
       assertEq(x, 1);
     }
@@ -49,25 +49,25 @@ contract SimpleTest is Test{
       assertFalse(x == 0);
     }
 
-    function prove_assume1(uint x) public pure {
+    function prove_assume_assert_true(uint x) public pure {
+      // Passes with `hevm symbolic --code $BYTECODE --sig "prove_assume2(uint)"`
+      // Passes with `hevm test`
+      vm.assume(x < 1);
+      assert(x == 0);
+    }
+
+    function prove_assume_assert_false(uint x) public pure {
       // Fails with `hevm symbolic --code $BYTECODE --sig "prove_assume1(uint)"`
       // Fails with `hevm test`
       vm.assume(x < 1);
       assert(x != 0);
     }
 
-    function proveFail_assume1(uint x) public pure {
+    function proveFail_assume_assert(uint x) public pure {
       // Fails with `hevm symbolic --code $BYTECODE --sig "proveFail_assume1(uint)"`
       // Passes with `hevm test`
       vm.assume(x < 1);
       assert(x != 0);
-    }
-
-    function prove_assume2(uint x) public pure {
-      // Passes with `hevm symbolic --code $BYTECODE --sig "prove_assume2(uint)"`
-      // Passes with `hevm test`
-      vm.assume(x < 1);
-      assert(x == 0);
     }
 
     function prove_expectRevert() public {
@@ -82,23 +82,24 @@ contract SimpleTest is Test{
       revert("Just reverts");
     }
 
-    function prove_simple(uint x) public pure {
-      // Passes with `hevm symbolic --code $BYTECODE --sig "prove_simple(uint)"`
+    function prove_require_assert_true(uint x) public pure {
+      // Passes with `hevm symbolic --code $BYTECODE --sig "prove_require_assert_true(uint)"`
       // Passes with `hevm test`
       require(x < 1);
       assert(x == 0);
     }
 
-    function prove_simple2(uint x) public pure {
-      // Fails with `hevm symbolic --code $BYTECODE --sig "prove_simple2(uint)"`
+    function prove_require_assert_false(uint x) public pure {
+      // Fails with `hevm symbolic --code $BYTECODE --sig "prove_require_assert_false(uint)"`
       // Fails with `hevm test`
       require(x < 1);
       assert(x != 0);
     }
 
-    function proveFail_simple2(uint x) public pure {
-      // Fails with `hevm symbolic --code $BYTECODE --sig "proveFail_simple2(uint)"`
+    function proveFail_require_assert(uint x) public pure {
+      // Fails with `hevm symbolic --code $BYTECODE --sig "proveFail_require_assert(uint)"`
       // Passes with `hevm test`
+      // Passes with `kontrol prove --match-test proveFail_require_assert`
       require(x < 1);
       assert(x != 0);
     }
