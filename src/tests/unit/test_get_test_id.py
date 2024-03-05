82c87e1496cff995611b4842e742e7a5730aafa8
Andrei <andrei.vacaru@runtimeverification.com>
2024-03-05 12:46:10 +0200

    test_get_test_id.py: update fixture and add new test case

--- src/tests/unit/test_get_test_id.py
+++ src/tests/unit/test_get_test_id.py
@@ -64,42 +64,57 @@ TEST_ID_DATA = [
         'with_version',
         'AssertTest.setUp()',
         0,
+        False,
         'test%AssertTest.setUp():0',
     ),
     (
         'without_version',
         'AssertTest.setUp()',
         None,
+        False,
         'test%AssertTest.setUp():1',
     ),
     (
-        'proof_not_found',
+        'no_matches',
         'AssertTest.setUp()',
         3,
-        'Found\\ no\\ matching\\ proofs\\ for\\ AssertTest\\.setUp\\(\\):3\\.',
+        True,
+        r'Found no matching proofs for AssertTest\.setUp\(\):3\.',
     ),
     (
-        'multiple_matches',
+        'multiple_matches_with_version',
         'AssertTest.test_assert',
         0,
-        'Found\\ 2\\ matching\\ proofs\\ for\\ AssertTest\\.test_assert:0\\.',
+        True,
+        r'Found 2 matching proofs for AssertTest\.test_assert:0\.',
+    ),
+    (
+        'multiple_matches_without_version',
+        'AssertTest.test_assert',
+        None,
+        True,
+        r'Found 2 matching proofs for AssertTest\.test_assert:None\.',
     ),
 ]
 
 
-@pytest.mark.parametrize('test_id,test,version,expected', TEST_ID_DATA, ids=[test_id for test_id, *_ in TEST_ID_DATA])
-def test_foundry_get_test_id(test_id: str, test: str, version: int | None, expected: str) -> None:
+@pytest.mark.parametrize(
+    'test_id,test,version,expect_error,expected_str', TEST_ID_DATA, ids=[test_id for test_id, *_ in TEST_ID_DATA]
+)
+def test_foundry_get_test_id(
+    test_id: str, test: str, version: int | None, expect_error: bool, expected_str: str
+) -> None:
 
     # Given
     foundry = cast('Foundry', FoundryMock())
 
-    if test_id in ['proof_not_found', 'multiple_matches']:
+    if expect_error:
         # When/Then
-        with pytest.raises(ValueError, match=expected):
+        with pytest.raises(ValueError, match=expected_str):
             foundry.get_test_id(test, version)
     else:
         # When
         id = foundry.get_test_id(test, version)
 
         # Then
-        assert id == expected
+        assert id == expected_str
