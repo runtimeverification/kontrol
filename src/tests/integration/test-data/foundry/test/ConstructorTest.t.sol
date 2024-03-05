cb3316478d4cd5ae8945c66aeff76652a59cc39a
qian-hu <88806138+qian-hu@users.noreply.github.com>
2024-03-05 16:01:39 -0500

    Add test for running contract init code when no constructor

---
+++ src/tests/integration/test-data/foundry/test/ConstructorTest.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.13;
+
+import "forge-std/Test.sol";
+
+contract ConstructorTest is Test {
+    bool flag = true;
+
+    function test_constructor() public {
+        assert(flag);
+    }
+
+    function testFail_constructor() public {
+        assert(!flag);
+    }
+}
