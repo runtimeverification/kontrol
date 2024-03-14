499ac0b37e9cf0835ff7c46c70b8bdc35ba8d7ea
Petar Maksimovic <petar.maksimovic@runtimeverification.com>
2024-03-14 18:00:22 +0000

    Updating the running of proofs

--- src/tests/integration/test_kontrol_cse.py
+++ src/tests/integration/test_kontrol_cse.py
@@ -102,6 +102,40 @@ def test_foundry_dependency_automated(
     if bug_report is not None:
         server._populate_bug_report(bug_report)
 
+    # Execution without CSE
+    prove_options = ProveOptions(
+        max_depth=10000,
+        max_iterations=100,
+        break_on_calls=False,
+        bug_report=bug_report,
+        fail_fast=False,
+        workers=2,
+    )
+
+    foundry_prove(
+        foundry,
+        tests=[(test_id, None)],
+        prove_options=prove_options,
+        rpc_options=RPCOptions(port=server.port, smt_timeout=500),
+    )
+
+    show_res = foundry_show(
+        foundry,
+        test=test_id,
+        to_module=False,
+        sort_collections=True,
+        omit_unstable_output=True,
+        pending=False,
+        failing=False,
+        failure_info=False,
+        counterexample_info=False,
+        port=server.port,
+    )
+
+    assert_or_update_show_output(
+        show_res, TEST_DATA_DIR / f'show/{test_id}.no-cse.expected', update=update_expected_output
+    )
+
     # Execution with CSE
     cse_prove_options = ProveOptions(
         max_depth=10000,
@@ -109,6 +143,7 @@ def test_foundry_dependency_automated(
         bug_report=bug_report,
         break_on_calls=False,
         cse=True,
+        reinit=True,
         fail_fast=False,
         workers=2,
     )
