871f883fae16057ab0acc04d5c5d64e1df3d17db
palinatolmach <polina.tolmach@gmail.com>
2024-03-15 15:07:12 +0800

    Formatting in tests

--- src/tests/integration/test_foundry_prove.py
+++ src/tests/integration/test_foundry_prove.py
@@ -82,7 +82,11 @@ def foundry(foundry_root_dir: Path | None, tmp_path_factory: TempPathFactory, wo
             foundry_kompile(
                 foundry=foundry,
                 includes=(),
-                requires=[str(TEST_DATA_DIR / 'lemmas.k'), str(TEST_DATA_DIR / 'cse-lemmas.k'), str(TEST_DATA_DIR / 'pausability-lemmas.k')],
+                requires=[
+                    str(TEST_DATA_DIR / 'lemmas.k'),
+                    str(TEST_DATA_DIR / 'cse-lemmas.k'),
+                    str(TEST_DATA_DIR / 'pausability-lemmas.k'),
+                ],
                 imports=['LoopsTest:SUM-TO-N-INVARIANT', 'AssertTest:CSE-LEMMAS', 'PortalTest:PAUSABILITY-LEMMAS'],
             )
 
