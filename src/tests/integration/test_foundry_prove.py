56833357990b4b9557297a61b616439ecbef50c0
Andrei <andrei.vacaru@runtimeverification.com>
2024-03-26 13:04:33 +0200

    test_foundry_prove.py: formatting

--- src/tests/integration/test_foundry_prove.py
+++ src/tests/integration/test_foundry_prove.py
@@ -12,7 +12,6 @@ from pyk.utils import single
 from kontrol.foundry import (
     Foundry,
     foundry_merge_nodes,
-    foundry_minimize_proof,
     foundry_refute_node,
     foundry_remove_node,
     foundry_show,
