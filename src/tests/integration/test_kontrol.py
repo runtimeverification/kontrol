53ca9199a39cfd1432b1018033e37ae89fd8ca1b
Andrei <andrei.vacaru@runtimeverification.com>
2024-05-13 16:34:06 +0300

    test_kontrol.py: fix server fixture

--- src/tests/integration/test_kontrol.py
+++ src/tests/integration/test_kontrol.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 
 import pytest
 from filelock import FileLock
+from pyk.kore.rpc import kore_server
 from pyk.utils import single
 
 from kontrol.foundry import Foundry, init_project
@@ -15,6 +16,7 @@ from kontrol.prove import ProveOptions, foundry_prove
 from .utils import TEST_DATA_DIR, assert_pass
 
 if TYPE_CHECKING:
+    from collections.abc import Iterator
     from pathlib import Path
     from typing import Final
 
@@ -25,14 +27,27 @@ if TYPE_CHECKING:
 
 sys.setrecursionlimit(10**7)
 
-ALL_PROVE_TESTS: Final = tuple((TEST_DATA_DIR / 'end-to-end-prove-all').read_text().splitlines())
+
+@pytest.fixture(scope='module')
+def server_end_to_end(foundry_end_to_end: Foundry, no_use_booster: bool) -> Iterator[KoreServer]:
+    llvm_definition_dir = foundry_end_to_end.out / 'kompiled' / 'llvm-library' if not no_use_booster else None
+    kore_rpc_command = ('kore-rpc-booster',) if not no_use_booster else ('kore-rpc',)
+
+    yield kore_server(
+        definition_dir=foundry_end_to_end.kevm.definition_dir,
+        llvm_definition_dir=llvm_definition_dir,
+        module_name=foundry_end_to_end.kevm.main_module,
+        command=kore_rpc_command,
+        smt_timeout=500,
+        smt_retry_limit=10,
+        fallback_on=None,
+        interim_simplification=None,
+        no_post_exec_simplify=None,
+    )
 
 
 @pytest.fixture(scope='session')
-def foundry_project(foundry_root_dir: Path | None, tmp_path_factory: TempPathFactory, worker_id: str) -> Foundry:
-    if foundry_root_dir:
-        return Foundry(foundry_root_dir)
-
+def foundry_end_to_end(foundry_root_dir: Path | None, tmp_path_factory: TempPathFactory, worker_id: str) -> Foundry:
     if worker_id == 'master':
         root_tmp_dir = tmp_path_factory.getbasetemp()
     else:
@@ -54,29 +69,32 @@ def foundry_project(foundry_root_dir: Path | None, tmp_path_factory: TempPathFac
     return Foundry(session_foundry_root)
 
 
+ALL_PROVE_TESTS: Final = set((TEST_DATA_DIR / 'end-to-end-prove-all').read_text().splitlines())
+
+
 @pytest.mark.parametrize('test_id', ALL_PROVE_TESTS)
 def test_kontrol_end_to_end(
     test_id: str,
-    foundry_project: Foundry,
+    foundry_end_to_end: Foundry,
     update_expected_output: bool,
     no_use_booster: bool,
     bug_report: BugReport | None,
-    server: KoreServer,
+    server_end_to_end: KoreServer,
 ) -> None:
 
     if bug_report is not None:
-        server._populate_bug_report(bug_report)
+        server_end_to_end._populate_bug_report(bug_report)
 
     # When
     prove_res = foundry_prove(
-        foundry=foundry_project,
+        foundry=foundry_end_to_end,
         options=ProveOptions(
             {
                 'tests': [(test_id, None)],
                 'bug_report': bug_report,
                 'break_on_calls': False,
                 'use_gas': False,
-                'port': server.port,
+                'port': server_end_to_end.port,
             }
         ),
     )
