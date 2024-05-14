808ae0ab008495e6432fde34fcab6b6aabd0944c
rv-jenkins <admin@runtimeverification.com>
2024-05-13 23:41:32 -0600

    Update dependency: deps/kevm_release (#557)
    * deps/kevm_release: Set Version 1.0.553
    
    * Set Version: 0.1.275
    
    * Sync Poetry files: kevm-pyk version 1.0.553
    
    * deps/k_release: sync release file version 7.0.60
    
    * flake.{nix,lock}: update Nix derivations
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kontrol";
 
   inputs = {
-    kevm.url = "github:runtimeverification/evm-semantics/v1.0.552";
+    kevm.url = "github:runtimeverification/evm-semantics/v1.0.553";
     nixpkgs.follows = "kevm/nixpkgs";
     nixpkgs-pyk.follows = "kevm/nixpkgs-pyk";
     k-framework.follows = "kevm/k-framework";
