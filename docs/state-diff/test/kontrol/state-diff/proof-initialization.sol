d051a585508a63ffdcb61a7355ab112c47d83d82
Juan C <juanconrod@protonmail.com>
2024-05-20 19:07:59 +0200

    remove openzeppelin-contracts dependency

--- docs/state-diff/test/kontrol/state-diff/proof-initialization.sol
+++ docs/state-diff/test/kontrol/state-diff/proof-initialization.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Test} from "forge-std/Test.sol";
 import {Counter} from "src/Counter.sol";
 import {RecordStateDiff} from "./record-state-diff/RecordStateDiff.sol";
-import {Strings} from "lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
-contract CounterBed is RecordStateDiff {
+contract CounterBed is Test, RecordStateDiff {
     Counter public counter;
 
     function counterBed() public recordStateDiff {
@@ -19,7 +19,7 @@ contract CounterBed is RecordStateDiff {
         for (uint8 i; i <= 9; ++i) {
             counter = new Counter();
             counter.setNumber(i);
-            save_address(string.concat("counter", Strings.toString(i)), address(counter));
+            save_address(string.concat("counter", vm.toString(i)), address(counter));
         }
     }
 }
