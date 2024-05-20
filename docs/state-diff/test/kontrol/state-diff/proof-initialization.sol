a0ef72bf21b0de3a94ca2c119f84875ac186ff46
Juan C <juanconrod@protonmail.com>
2024-05-20 16:48:42 +0200

    `CounterBed`: add new test and cleanup

--- docs/state-diff/test/kontrol/state-diff/proof-initialization.sol
+++ docs/state-diff/test/kontrol/state-diff/proof-initialization.sol
@@ -1,18 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "src/Counter.sol";
 import {RecordStateDiff} from "./record-state-diff/RecordStateDiff.sol";
+import {Strings} from "lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
 contract CounterBed is RecordStateDiff {
     Counter public counter;
 
     function counterBed() public recordStateDiff {
-        for(uint8 i; i <=9; ++i) {
+        for (uint8 i; i <= 9; ++i) {
             counter = new Counter();
             counter.setNumber(i);
         }
     }
 
+    function counterBedNamed() public recordStateDiff {
+        for (uint8 i; i <= 9; ++i) {
+            counter = new Counter();
+            counter.setNumber(i);
+            save_address(string.concat("counter", Strings.toString(i)), address(counter));
+        }
+    }
 }
