b398381cccdd72c1ddc7055fa8b27204ffd689ae
palinatolmach <polina.tolmach@gmail.com>
2024-05-20 19:10:40 +0800

    Add `ImmutableVarsTest` for with symbolic constructor args, lemmas

---
+++ src/tests/integration/test-data/foundry/test/ImmutableVarsTest.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.13;
+
+import "forge-std/Test.sol";
+import "kontrol-cheatcodes/KontrolCheats.sol";
+
+contract ImmutableVarsContract {
+    uint256 public immutable y;
+
+    constructor(uint256 _y) {
+        y = _y;
+    }
+}
+
+contract ImmutableVarsTest is Test {
+    function test_run_deployment(uint256 x) public returns (bool) {
+        ImmutableContract c = new ImmutableContract(x);
+        assert(c.y() == 85);
+    }
+}
