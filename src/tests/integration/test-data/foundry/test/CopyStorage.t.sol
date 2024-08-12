1c1e7fd9507652e640368b8483dba30220fdd5aa
Petar Maksimovic <petar.maksimovic@runtimeverification.com>
2024-08-12 22:48:05 +0200

    syntax

--- src/tests/integration/test-data/foundry/test/CopyStorage.t.sol
+++ src/tests/integration/test-data/foundry/test/CopyStorage.t.sol
@@ -12,6 +12,10 @@ contract CopyStorageTest is Test, KontrolCheats {
     CopyStorageContract csc_1;
     CopyStorageContract csc_2;
 
+    function _storeUInt256(address contractAddress, uint256 slot, uint256 value) internal {
+        vm.store(contractAddress, bytes32(slot), bytes32(value));
+    }
+
     function setUp() public {
         csc_1 = new CopyStorageContract();
         csc_2 = new CopyStorageContract();
@@ -19,15 +23,15 @@ contract CopyStorageTest is Test, KontrolCheats {
 
     function testCopyStorage() public {
         // Make the storage of first contract symbolic
-        kevm.symbolicStorage(csc_1);
+        kevm.symbolicStorage(address(csc_1));
         // and explicitly put a constrained symbolic value into the slot for `x`
         uint256 x_1 = uint256(kevm.freshUInt(32));
-        _storeUInt256(address(x_1), 0, x_1);
+        _storeUInt256(address(csc_1), 0, x_1);
 
         // `x` of second contract is uninitialized
         assert(csc_2.x() == 0);
         // Copy storage from first to second contract
-        kevm.copyStorage(address(csc1), address(csc2));
+        kevm.copyStorage(address(csc_1), address(csc_2));
         // `x` of second contract is now the `x` of the first
         assert(csc_2.x() == x_1);
     }
