96be36c7bb963f32c7c4128d7c5c9ac1dd6b0b6d
palinatolmach <polina.tolmach@gmail.com>
2024-09-07 15:36:59 +0400

    Fix `foundry.toml` update

--- src/tests/integration/test_kontrol.py
+++ src/tests/integration/test_kontrol.py
@@ -13,7 +13,7 @@ from kontrol.foundry import Foundry, init_project
 from kontrol.kompile import foundry_kompile
 from kontrol.options import BuildOptions, ProveOptions
 from kontrol.prove import foundry_prove
-from kontrol.utils import foundry_toml_cancun_schedule, write_to_file
+from kontrol.utils import append_to_file, foundry_toml_cancun_schedule
 
 from .utils import TEST_DATA_DIR, assert_pass
 
@@ -60,7 +60,7 @@ def foundry_end_to_end(foundry_root_dir: Path | None, tmp_path_factory: TempPath
         if not foundry_root.is_dir():
             init_project(project_root=foundry_root, skip_forge=False)
             copy_tree(str(TEST_DATA_DIR / 'src'), str(foundry_root / 'test'))
-            write_to_file(TEST_DATA_DIR / 'foundry.toml', foundry_toml_cancun_schedule())
+            append_to_file(foundry_root / 'foundry.toml', foundry_toml_cancun_schedule())
 
             foundry_kompile(
                 BuildOptions(
