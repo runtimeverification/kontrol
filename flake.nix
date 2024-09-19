098070a4b53721c5e218c1a89b68bf14fd492044
rv-jenkins <admin@runtimeverification.com>
2024-09-19 08:43:04 -0600

    Update dependency: deps/kevm_release (#812)
    * deps/kevm_release: Set Version 1.0.712
    
    * Sync Poetry files: kevm-pyk version 1.0.712
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.713
    
    * Sync Poetry files: kevm-pyk version 1.0.713
    
    * deps/k_release: sync release file version 7.1.136
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.714
    
    * Sync Poetry files: kevm-pyk version 1.0.714
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.715
    
    * Sync Poetry files: kevm-pyk version 1.0.715
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.716
    
    * Sync Poetry files: kevm-pyk version 1.0.716
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.717
    
    * Sync Poetry files: kevm-pyk version 1.0.717
    
    * deps/k_release: sync release file version 7.1.139
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.718
    
    * Sync Poetry files: kevm-pyk version 1.0.718
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.719
    
    * Sync Poetry files: kevm-pyk version 1.0.719
    
    * flake.{nix,lock}: update Nix derivations
    
    * removing chop reasoning that has been upstreamed
    
    * Sync Poetry files: kevm-pyk version 1.0.719
    
    * deps/kevm_release: Set Version 1.0.720
    
    * Sync Poetry files: kevm-pyk version 1.0.720
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.721
    
    * Sync Poetry files: kevm-pyk version 1.0.721
    
    * deps/k_release: sync release file version 7.1.142
    
    * flake.{nix,lock}: update Nix derivations
    
    * Sync Poetry files: kevm-pyk version 1.0.721
    
    * deps/kevm_release: Set Version 1.0.722
    
    * Sync Poetry files: kevm-pyk version 1.0.722
    
    * flake.{nix,lock}: update Nix derivations
    
    * clean-up of asWord-related simplifications
    
    * Sync Poetry files: kevm-pyk version 1.0.722
    
    * Sync Poetry files: kevm-pyk version 1.0.722
    
    * update expected output
    
    * deps/kevm_release: Set Version 1.0.724
    
    * Sync Poetry files: kevm-pyk version 1.0.724
    
    * deps/k_release: sync release file version 7.1.143
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.725
    
    * Sync Poetry files: kevm-pyk version 1.0.725
    
    * flake.{nix,lock}: update Nix derivations
    
    * some expected outputs
    
    * deps/kevm_release: Set Version 1.0.726
    
    * Sync Poetry files: kevm-pyk version 1.0.726
    
    * deps/k_release: sync release file version 7.1.145
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.727
    
    * Sync Poetry files: kevm-pyk version 1.0.727
    
    * flake.{nix,lock}: update Nix derivations
    
    * CSE and minimize tests
    
    * other expected outputs
    
    * different output
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Petar Maksimovic <petar.maksimovic@runtimeverification.com>
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
    Co-authored-by: Petar Maksimović <PetarMax@users.noreply.github.com>
    Co-authored-by: Andrei <16517508+anvacaru@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kontrol";
 
   inputs = {
-    kevm.url = "github:runtimeverification/evm-semantics/v1.0.711";
+    kevm.url = "github:runtimeverification/evm-semantics/v1.0.727";
     nixpkgs.follows = "kevm/nixpkgs";
     k-framework.follows = "kevm/k-framework";
     flake-utils.follows = "kevm/flake-utils";
