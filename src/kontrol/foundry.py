44662ee3eed79df2d0a77210a1da728e2ef95484
palinatolmach <polina.tolmach@gmail.com>
2024-09-23 11:52:48 +0800

    toml: use `default` profile values for keys absent from the current profile

--- src/kontrol/foundry.py
+++ src/kontrol/foundry.py
@@ -182,7 +182,11 @@ class Foundry:
     @property
     def profile(self) -> dict[str, Any]:
         profile_name = os.getenv('FOUNDRY_PROFILE', default='default')
-        return self._toml['profile'][profile_name]
+        
+        current_profile = self._toml['profile'].get(profile_name, {})
+        default_profile = self._toml['profile'].get('default', {})
+        
+        return {**default_profile, **current_profile}
 
     @property
     def out(self) -> Path:
