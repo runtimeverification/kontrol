1889314a5a6e61eaaf35ee18380471e719d549fb
JianhongZhao <zhaojianhong96@gmail.com>
2024-09-24 12:05:05 +0800

    comment

--- src/tests/integration/test-data/foundry/test/MergeKCFG.t.sol
+++ src/tests/integration/test-data/foundry/test/MergeKCFG.t.sol
@@ -11,6 +11,7 @@ contract MergeKCFGTest is Test {
         try c.f(x, y, z) returns (uint256 res) {
             // This check will fail if the backend cannot recover the preds in the merged postcondition
             // If so, assert res == x + y | res == x * y will pass
+            // If so, we should not always use merge_node, but provide both on need.
             if (z) {
                 assert(res == x + y);
             } else {
