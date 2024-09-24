9dd8b535eccb4e778b13a150bf433bd762eebd8d
JianhongZhao <zhaojianhong96@gmail.com>
2024-09-24 09:29:23 +0800

    update copytree

--- src/tests/integration/conftest.py
+++ src/tests/integration/conftest.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from distutils.dir_util import copy_tree
+from shutil import copytree
 from functools import partial
 from typing import TYPE_CHECKING
 
@@ -64,7 +64,7 @@ def foundry(foundry_root_dir: Path | None, tmp_path_factory: TempPathFactory, wo
     foundry_root = root_tmp_dir / 'foundry'
     with FileLock(str(foundry_root) + '.lock'):
         if not foundry_root.is_dir():
-            copy_tree(str(TEST_DATA_DIR / 'foundry'), str(foundry_root))
+            copytree(str(TEST_DATA_DIR / 'foundry'), str(foundry_root))
 
             run_process_2(['forge', 'install', '--no-git', f'foundry-rs/forge-std@{FORGE_STD_REF}'], cwd=foundry_root)
             run_process_2(
@@ -98,5 +98,5 @@ def foundry(foundry_root_dir: Path | None, tmp_path_factory: TempPathFactory, wo
             )
 
     session_foundry_root = tmp_path_factory.mktemp('foundry')
-    copy_tree(str(foundry_root), str(session_foundry_root))
+    copytree(str(foundry_root), str(session_foundry_root))
     return Foundry(session_foundry_root, add_enum_constraints=True)
