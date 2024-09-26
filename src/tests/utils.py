182461c0ba5cfe26544efc39b875e8d6a6513544
JianhongZhao <zhaojianhong96@gmail.com>
2024-09-26 10:24:11 +0800

    replace copy_tree by copytree for py3.12

--- src/tests/utils.py
+++ src/tests/utils.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from distutils.dir_util import copy_tree
+from shutil import copytree
 from typing import TYPE_CHECKING
 
 from pyk.utils import run_process_2
@@ -17,7 +17,7 @@ FORGE_STD_REF: Final = '75f1746'
 
 
 def forge_build(test_data_dir: Path, target_dir: Path) -> Foundry:
-    copy_tree(str(test_data_dir / 'foundry'), str(target_dir))
+    copytree(str(test_data_dir / 'foundry'), str(target_dir))
     run_process_2(['forge', 'install', '--no-git', f'foundry-rs/forge-std@{FORGE_STD_REF}'], cwd=target_dir)
     run_process_2(['forge', 'build'], cwd=target_dir)
     return Foundry(foundry_root=target_dir)
