22b2fb5627ae2510a9e736cf8fb5cc94efc4c09f
Andrei <16517508+anvacaru@users.noreply.github.com>
2024-09-29 02:58:02 +0300

    update expected output

--- src/tests/integration/test-data/foundry/test/ExternalLibTest.t.sol
+++ src/tests/integration/test-data/foundry/test/ExternalLibTest.t.sol
@@ -11,7 +11,7 @@ library SimpleMath {
 }
 
 contract ExternalLibTest is Test {
-    function testSquare(uint256 n) public view{
+    function testSquare(uint256 n) public {
         vm.assume(msg.sender == address(110));
         vm.assume(n <= type(uint128).max);
         assertEq(SimpleMath.square(n), n ** 2);
