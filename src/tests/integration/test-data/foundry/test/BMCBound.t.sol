77f123c318f9f5021ffb6821ca91fd31d9982106
palinatolmach <polina.tolmach@gmail.com>
2024-10-03 22:42:40 +0800

    Add `BMCBoundTest` and its expected output

---
+++ src/tests/integration/test-data/foundry/test/BMCBound.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {KontrolCheats} from "kontrol-cheatcodes/KontrolCheats.sol";
+
+contract BMCBoundTest is Test, KontrolCheats {
+    uint x;
+
+    function setUp() public {
+        uint256 i = freshUInt256();
+        for (uint j = 0; j < i; j++) {
+            x += 1;
+        }
+    }
+
+    function testBound() public {
+        assertLe(x, 3);
+    }
+}
