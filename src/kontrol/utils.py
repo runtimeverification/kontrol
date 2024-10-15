f99026a68e97b134dc10139a5f6946176664502d
Andrei <16517508+anvacaru@users.noreply.github.com>
2024-10-15 18:06:10 +0300

    add no stack changes

--- src/kontrol/utils.py
+++ src/kontrol/utils.py
@@ -230,6 +230,7 @@ break-on-storage           = false
 break-on-basic-blocks      = false
 break-on-cheatcodes        = false
 run-constructor            = false
+no-stack-changes           = true
 
 [show.default]
 foundry-project-root       = '.'
