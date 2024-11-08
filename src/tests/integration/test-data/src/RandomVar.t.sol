5c66e131eb3e1b09b0cb2f130829333120b5aa89
palinatolmach <polina.tolmach@gmail.com>
2024-11-08 23:12:50 +0700

    Add `random*` tests to end-to-end tests

--- src/tests/integration/test-data/src/RandomVar.t.sol
+++ src/tests/integration/test-data/src/RandomVar.t.sol
@@ -4,23 +4,70 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 
 contract RandomVarTest is Test {
-    function test_randomBool() public {
+
+    uint256 constant length_limit = 72;
+
+    function test_randomBool() public view {
         bool freshBool = vm.randomBool();
         vm.assume(freshBool);
     }
 
     function test_randomAddress() public {
-        address fresh_address = vm.randomAddress();
-        assertNotEq(fresh_address, address(this));
-        assertNotEq(fresh_address, address(vm));
+        address freshAddress = vm.randomAddress();
+        assertNotEq(freshAddress, address(this));
+        assertNotEq(freshAddress, address(vm));
     }
 
-    function test_randomUints(uint256 x) public {
+    function test_randomUint(uint256 x) public view {
         vm.assume(0 < x);
         vm.assume(x <= 256);
         uint256 freshUint = vm.randomUint(x);
 
         assert(0 <= freshUint);
+        assert(freshUint <= 2 ** x - 1);
+    }
+
+    function test_randomUint() public {
+        uint256 freshUint = vm.randomUint();
+
+        assert(0 <= freshUint);
         assert(freshUint <= 2 ** 256 - 1);
+    }
+
+    function test_randomInt(uint256 x) public view {
+        vm.assume(0 < x);
+        vm.assume(x <= 256);
+
+        int256 minInt256 = -2 ** (x - 1);
+        int256 maxInt256 = -2 ** (x - 1);
+
+        int256 freshInt = vm.randomInt(x);
+
+        assert(minInt256 <= freshInt);
+        assert(freshInt <= maxInt256);
+    }
+
+    function test_randomInt() public view {
+        int256 freshInt = vm.randomInt();
+
+        assert(-2 ** 255 <= freshInt);
+        assert(freshInt <= 2 ** 255 - 1);
+    }
+
+    function test_randomBytes_length(uint256 len) public view {
+        vm.assume(0 < len);
+        vm.assume(len <= length_limit);
+        bytes memory freshBytes = vm.randomBytes(len);
+        assertEq(freshBytes.length, len);
+    }
+
+    function test_randomBytes4_length() public view {
+        bytes8 freshBytes = vm.randomBytes4();
+        assertEq(freshBytes.length, 4);
+    }
+
+    function test_randomBytes8_length() public view {
+        bytes8 fresh_bytes = vm.randomBytes8();
+        assertEq(fresh_bytes.length, 8);
     }
 }
