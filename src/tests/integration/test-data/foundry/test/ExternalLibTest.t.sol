76fe7c8f930c990b08436d4a310139ad0f048474
palinatolmach <polina.tolmach@gmail.com>
2024-12-15 23:50:06 +0400

    Add a second test to `ExternalLibTest`

--- src/tests/integration/test-data/foundry/test/ExternalLibTest.t.sol
+++ src/tests/integration/test-data/foundry/test/ExternalLibTest.t.sol
@@ -16,6 +16,10 @@ library SimpleMath {
     function square(uint256 x) public pure returns (uint256) {
         return x * x;
     }
+
+    function sum(uint256 a, uint256 b) external pure returns (uint256 res) {
+        res = a + b;
+    }
 }
 
 contract ExternalLibTest is Test {
@@ -23,5 +27,11 @@ contract ExternalLibTest is Test {
         vm.assume(msg.sender == address(110));
         vm.assume(n <= type(uint128).max);
         assertEq(SimpleMath.square(n), n * n);
+    }
+
+    function testSum(uint256 x, uint256 y) public {
+        vm.assume(x <= type(uint128).max);
+        vm.assume(y <= type(uint128).max);
+        assertEq(SimpleMath.sum(x, y), 10);
     }
 }
