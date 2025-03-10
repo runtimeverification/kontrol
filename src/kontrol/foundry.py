23152bc2c3eb41d7b7d9cf50b5c86e15abe6c3ec
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2025-03-10 17:57:49 +0000

    src/kontrol/foundry: enable extra lemmas for all commands involving RPC server

--- src/kontrol/foundry.py
+++ src/kontrol/foundry.py
@@ -1043,6 +1043,7 @@ def foundry_show(
             smt_retry_limit=options.smt_retry_limit,
             start_server=start_server,
             port=options.port,
+            extra_module=foundry.load_lemmas(options.lemmas),
         ) as kcfg_explore:
             res_lines += print_failure_info(proof, kcfg_explore, options.counterexample_info)
             res_lines += Foundry.help_info()
@@ -1306,6 +1307,7 @@ def foundry_simplify_node(
         log_fail_rewrites=options.log_fail_rewrites,
         start_server=start_server,
         port=options.port,
+        extra_module=foundry.load_lemmas(options.lemmas),
     ) as kcfg_explore:
         new_term, _ = kcfg_explore.cterm_symbolic.simplify(cterm)
     if options.replace:
@@ -1394,6 +1396,7 @@ def foundry_step_node(
         log_fail_rewrites=options.log_fail_rewrites,
         start_server=start_server,
         port=options.port,
+        extra_module=foundry.load_lemmas(options.lemmas),
     ) as kcfg_explore:
         node = options.node
         for _i in range(options.repeat):
@@ -1470,6 +1473,7 @@ def foundry_section_edge(
         log_fail_rewrites=options.log_fail_rewrites,
         start_server=start_server,
         port=options.port,
+        extra_module=foundry.load_lemmas(options.lemmas),
     ) as kcfg_explore:
         kcfg_explore.section_edge(
             apr_proof.kcfg,
@@ -1521,6 +1525,7 @@ def foundry_get_model(
         log_fail_rewrites=options.log_fail_rewrites,
         start_server=start_server,
         port=options.port,
+        extra_module=foundry.load_lemmas(options.lemmas),
     ) as kcfg_explore:
         for node_id in nodes:
             res_lines.append('')
