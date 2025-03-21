1182c70189c8634ea3abdf8eb6199a1649b7d769
rv-jenkins <admin@runtimeverification.com>
2025-03-21 09:42:50 -0600

    Update dependency: deps/kevm_release (#997)
    * deps/kevm_release: Set Version 1.0.811
    
    * Sync Poetry files: kevm-pyk version 1.0.811
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/kevm_release: Set Version 1.0.812
    
    * Sync Poetry files: kevm-pyk version 1.0.812
    
    * deps/k_release: sync release file version 7.1.230
    
    * flake.{nix,lock}: update Nix derivations
    
    * Sync Poetry files: kevm-pyk version 1.0.812
    
    * Remove `macos-13` runners from Nix tests
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Palina <polina.tolmach@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kontrol";
 
   inputs = {
-    kevm.url = "github:runtimeverification/evm-semantics/v1.0.810";
+    kevm.url = "github:runtimeverification/evm-semantics/v1.0.812";
     nixpkgs.follows = "kevm/nixpkgs";
     k-framework.follows = "kevm/k-framework";
     flake-utils.follows = "kevm/flake-utils";
