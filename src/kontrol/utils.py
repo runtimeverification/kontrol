62aa7bb1e260aaa1c0316de116af972a48297b0e
Palina <palina.tolmach@runtimeverification.com>
2025-09-30 12:27:48 +0100

    Add `KontrolTest` generation to `kontrol init` (#1081)
    * Add `KontrolTest` to `kontrol init`
    
    * Formatting
    
    * Update argument description for `--skip-kontrol-test`
    
    * Update formatting in `KontrolTest`
    
    * Formatting: quotes update
    
    * Prevent failing on already installed `kontrol-cheatcodes`
    
    * Update src/kontrol/utils.py
    
    Co-authored-by: Andrei Văcaru <16517508+anvacaru@users.noreply.github.com>
    
    * Change `assert` to `require`
    
    * Add checks on `width` in `_storeData`
    
    Co-authored-by: lucasmt <36549752+lucasmt@users.noreply.github.com>
    
    * Remove `width == 32` case handling
    
    ---------
    
    Co-authored-by: Andrei Văcaru <16517508+anvacaru@users.noreply.github.com>
    Co-authored-by: lucasmt <36549752+lucasmt@users.noreply.github.com>

--- src/kontrol/utils.py
+++ src/kontrol/utils.py
@@ -716,3 +716,142 @@ CONSOLE_SELECTORS: Final[dict[int, list[str]]] = {
     238520724: ['address', 'address', 'address', 'bool'],
     1717301556: ['address', 'address', 'address', 'address'],
 }
+
+
+def kontrol_test_file_contents() -> str:
+    return """pragma solidity ^0.8.26;
+import "forge-std/Vm.sol";
+import "forge-std/Test.sol";
+import "kontrol-cheatcodes/KontrolCheats.sol";
+
+contract KontrolTest is Test, KontrolCheats {
+    enum Mode {
+        Assume,
+        Try,
+        Assert
+    }
+    function _establish(Mode mode, bool condition) internal pure returns (bool) {
+        if (mode == Mode.Assume) {
+            vm.assume(condition);
+            return true;
+        } else if (mode == Mode.Try) {
+            return condition;
+        } else {
+            assert(condition);
+            return true;
+        }
+    }
+    function _loadData(
+        address contractAddress,
+        uint256 slot,
+        uint256 offset,
+        uint256 width
+    ) internal view returns (uint256) {
+        require(contractAddress != address(0), 'Invalid contract address');
+        require(width > 0, 'Width must be greater than 0');
+        // `offset` and `width` must not overflow the slot
+        require(offset + width <= 32, "Offset + width exceeds slot size");
+        
+        // Slot read mask; if (2 ** (8 * width)) overflows, (2 ** (8 * width)) - 1 will equal type(uint256).max
+        uint256 mask;
+        unchecked {
+            mask = (2 ** (8 * width)) - 1;
+        }
+        // Value right shift
+        uint256 shift = 8 * offset;
+        // Current slot value
+        uint256 slotValue = uint256(vm.load(contractAddress, bytes32(slot)));
+        // Isolate and return data to retrieve
+        return mask & (slotValue >> shift);
+    }
+    function _storeData(address contractAddress, uint256 slot, uint256 offset, uint256 width, uint256 value) internal {
+        require(contractAddress != address(0), 'Invalid contract address');
+        require(width > 0, 'Width must be greater than 0');
+        // `offset` and `width` must not overflow the slot
+        require(offset + width <= 32, "Offset + width exceeds slot size");
+        // and `value` must fit into the designated part
+        require(width == 32 || value < 2 ** (8 * width), "Value exceeds designated part");
+        // Slot update mask; if (2 ** (8 * width)) overflows, (2 ** (8 * width)) - 1 will equal type(uint256).max
+        uint256 maskLeft;
+        unchecked {
+            maskLeft = ~((2 ** (8 * (offset + width))) - 1);
+        }
+        uint256 maskRight = (2 ** (8 * offset)) - 1;
+        uint256 mask = maskLeft | maskRight;
+        uint256 updatedValue = (2 ** (8 * offset)) * value;
+        // Current slot value
+        uint256 slotValue = uint256(vm.load(contractAddress, bytes32(slot)));
+        // Updated slot value
+        slotValue = updatedValue | (mask & slotValue);
+        vm.store(contractAddress, bytes32(slot), bytes32(slotValue));
+    }
+    function _loadMappingData(
+        address contractAddress,
+        uint256 mappingSlot,
+        uint256 key,
+        uint256 subSlot,
+        uint256 offset,
+        uint256 width
+    ) internal view returns (uint256) {
+        bytes32 hashedSlot = keccak256(abi.encodePacked(key, mappingSlot));
+        return _loadData(contractAddress, uint256(hashedSlot) + subSlot, offset, width);
+    }
+    function _storeMappingData(
+        address contractAddress,
+        uint256 mappingSlot,
+        uint256 key,
+        uint256 subSlot,
+        uint256 offset,
+        uint256 width,
+        uint256 value
+    ) internal {
+        bytes32 hashedSlot = keccak256(abi.encodePacked(key, mappingSlot));
+        _storeData(contractAddress, uint256(hashedSlot) + subSlot, offset, width, value);
+    }
+    function _loadUInt256(address contractAddress, uint256 slot) internal view returns (uint256) {
+        return _loadData(contractAddress, slot, 0, 32);
+    }
+    function _loadMappingUInt256(
+        address contractAddress,
+        uint256 mappingSlot,
+        uint256 key,
+        uint256 subSlot
+    ) internal view returns (uint256) {
+        bytes32 hashedSlot = keccak256(abi.encodePacked(key, mappingSlot));
+        return _loadData(contractAddress, uint256(hashedSlot) + subSlot, 0, 32);
+    }
+    function _loadAddress(address contractAddress, uint256 slot) internal view returns (address) {
+        return address(uint160(_loadData(contractAddress, slot, 0, 20)));
+    }
+    function _storeMappingUInt256(
+        address contractAddress,
+        uint256 mappingSlot,
+        uint256 key,
+        uint256 subSlot,
+        uint256 value
+    ) internal {
+        bytes32 hashedSlot = keccak256(abi.encodePacked(key, mappingSlot));
+        _storeData(contractAddress, uint256(hashedSlot) + subSlot, 0, 32, value);
+    }
+    function _storeUInt256(address contractAddress, uint256 slot, uint256 value) internal {
+        _storeData(contractAddress, slot, 0, 32, value);
+    }
+    function _storeAddress(address contractAddress, uint256 slot, address value) internal {
+        _storeData(contractAddress, slot, 0, 20, uint160(value));
+    }
+    function _storeBytes32(address contractAddress, uint256 slot, bytes32 value) internal {
+        _storeUInt256(contractAddress, slot, uint256(value));
+    }
+    function _assumeNoOverflow(uint256 augend, uint256 addend) internal pure {
+        unchecked {
+            vm.assume(augend < augend + addend);
+        }
+    }
+    function _clearSlot(address contractAddress, uint256 slot) internal {
+        _storeUInt256(contractAddress, slot, 0);
+    }
+    function _clearMappingSlot(address contractAddress, uint256 mappingSlot, uint256 key, uint256 subSlot) internal {
+        bytes32 hashedSlot = keccak256(abi.encodePacked(key, mappingSlot));
+        _storeData(contractAddress, uint256(hashedSlot) + subSlot, 0, 32, 0);
+    }
+}"""
